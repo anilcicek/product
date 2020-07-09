a5019679b7d61ade9f1fafa5702ffc1d914363e4
Anil cicek <anil.cicek@etiya.com>
2020-07-09 04:30:36 +0300

    Unit test classes

---
+++ src/test/java/com/anil/product/command/SearchProductCommandTest.java
@@ -0,0 +1,21 @@
+package com.anil.product.command;
+
+import com.anil.product.base.GeneralEnumaration;
+import com.anil.product.entity.Product;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+class SearchProductCommandTest {
+
+    @Test
+    void processCommand() {
+
+
+
+    }
+
+}
